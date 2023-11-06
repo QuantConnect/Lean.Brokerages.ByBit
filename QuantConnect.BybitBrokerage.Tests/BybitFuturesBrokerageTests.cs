2ec489287fafe43517c5e7cc3b46372ac383f4e6
Sebastian Karasek <sebastian@karasek.io>
2023-11-06 14:11:35 +0100

    Fix tests and small refactoring to avoid duplicate code (#5)
    * Fix and cleanup tests
    
    * Address review

--- QuantConnect.BybitBrokerage.Tests/BybitFuturesBrokerageTests.cs
+++ QuantConnect.BybitBrokerage.Tests/BybitFuturesBrokerageTests.cs
@@ -1,53 +1,33 @@
-﻿using Moq;
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 using NUnit.Framework;
-using QuantConnect.Brokerages;
-using QuantConnect.BybitBrokerage.Api;
 using QuantConnect.BybitBrokerage.Models.Enums;
-using QuantConnect.Configuration;
-using QuantConnect.Interfaces;
-using QuantConnect.Lean.Engine.DataFeeds;
-using QuantConnect.Securities;
+using QuantConnect.Orders.Fees;
 using QuantConnect.Tests.Brokerages;
 
 namespace QuantConnect.BybitBrokerage.Tests;
 
-
 [TestFixture, Explicit("Requires valid credentials to be setup and run outside USA")]
-public partial class BybitFuturesBrokerageTests : BrokerageTests
+public partial class BybitFuturesBrokerageTests : BybitBrokerageTests
 {
-    protected static Symbol BTCUSDT = Symbol.Create("BTCUSDT", SecurityType.CryptoFuture, "bybit");
-    private BybitApi _client;
+    private static Symbol BTCUSDT = Symbol.Create("BTCUSDT", SecurityType.CryptoFuture, "bybit");
     protected override Symbol Symbol { get; } = BTCUSDT;
     protected override SecurityType SecurityType => SecurityType.Future;
-    protected virtual ISymbolMapper SymbolMapper => new SymbolPropertiesDatabaseSymbolMapper(Market.Bybit);
+    protected override BybitProductCategory Category => BybitProductCategory.Linear;
     protected override decimal GetDefaultQuantity() => 0.001m;
-
-    protected override IBrokerage CreateBrokerage(IOrderProvider orderProvider, ISecurityProvider securityProvider)
-    {
-        var algorithm = new Mock<IAlgorithm>();
-        var apiKey = Config.Get("bybit-api-key");
-        var apiSecret = Config.Get("bybit-api-secret");
-        var apiUrl = Config.Get("bybit-api-url", "https://api-testnet.bybit.com");
-        var websocketUrl = Config.Get("bybit-websocket-url", "wss://stream-testnet.bybit.com");
-
-        _client = CreateRestApiClient(apiKey, apiSecret, apiUrl);
-        return new BybitBrokerage(apiKey, apiSecret, apiUrl, websocketUrl, algorithm.Object, orderProvider,
-            securityProvider, new AggregationManager(), null);
-    }
-
-    protected virtual BybitApi CreateRestApiClient(string apiKey, string apiSecret, string apiUrl)
-    {
-        return new BybitApi(SymbolMapper, null, apiKey, apiSecret, apiUrl);
-    }
-
-
-    protected override bool IsAsync() => false;
-
-    protected override decimal GetAskPrice(Symbol symbol)
-    {
-        var brokerageSymbol = SymbolMapper.GetBrokerageSymbol(symbol);
-        return _client.Market.GetTicker(BybitProductCategory.Linear, brokerageSymbol).Ask1Price!.Value;
-    }
 
 
     /// <summary>
@@ -63,15 +43,6 @@ public partial class BybitFuturesBrokerageTests : BrokerageTests
             new TestCaseData(new StopLimitOrderTestParameters(BTCUSDT, 50000m, 10000m)).SetName("StopLimitOrder"),
             new TestCaseData(new LimitIfTouchedOrderTestParameters(BTCUSDT, 50000m, 20000)).SetName(
                 "LimitIfTouchedOrder")
-        };
-    }
-
-    private static Symbol[] InverseSymbols()
-    {
-        return new[]
-        {
-            QuantConnect.Symbol.Create("BTCUSD", SecurityType.CryptoFuture, Market.Bybit),
-            QuantConnect.Symbol.Create("BTCUSDZ23", SecurityType.CryptoFuture, Market.Bybit)
         };
     }
 
@@ -117,24 +88,9 @@ public partial class BybitFuturesBrokerageTests : BrokerageTests
         base.LongFromShort(parameters);
     }
 
-    [Test, TestCaseSource(nameof(InverseSymbols))]
-    public void InversePairNotSupported(Symbol symbol)
+    [Ignore("The brokerage is shared between different product categories, therefore this test is only required in the base class")]
+    public override void GetAccountHoldings()
     {
-        var parameter = new MarketOrderTestParameters(symbol);
-        var order = parameter.CreateLongOrder(GetDefaultQuantity());
-
-        BrokerageMessageEvent brokerageMessageEvent = null;
-
-        void OnBrokerageOnMessage(object sender, BrokerageMessageEvent @event)
-        {
-            brokerageMessageEvent = @event;
-            Brokerage.Message -= OnBrokerageOnMessage;
-        }
-
-        Brokerage.Message += OnBrokerageOnMessage;
-
-        Assert.IsFalse(Brokerage.PlaceOrder(order));
-        Assert.IsNotNull(brokerageMessageEvent);
-        Assert.AreEqual($"Symbol is not supported {order.Symbol}", brokerageMessageEvent.Message);
+        base.GetAccountHoldings();
     }
 }
